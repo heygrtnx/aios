ec15271b727c3fa2f69a7ad9891935e8c7526abf
heygrtnx <thegrtnx@duck.com>
2026-02-18 01:32:34 +0100

    🚧WIP: prisma setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LibModule } from '../lib/lib.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    LibModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/lib/prisma/prisma.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prismaPath';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor(config: ConfigService) {
+    super({
+      adapter: new PrismaPg({
+        connectionString: config.get<string>('DATABASE_URL')!,
+      }),
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+    console.log('✅ Prisma connected');
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
